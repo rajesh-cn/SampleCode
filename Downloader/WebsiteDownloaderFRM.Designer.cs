4fe8a2318d629f45964f42f5cd158e42a79a1149
Rajesh Tailor <rajesh.tailor@arkasoftwares.com>
2021-12-10 15:08:41 +0530

    Initial Commit for SAMPLE Code

---
+++ Downloader/WebsiteDownloaderFRM.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Downloader
+{
+    partial class WebsiteDownloaderFRM
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadBTN = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cancelBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // downloadBTN
+            // 
+            this.downloadBTN.AllowDrop = true;
+            this.downloadBTN.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.downloadBTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.downloadBTN.Location = new System.Drawing.Point(22, 22);
+            this.downloadBTN.Name = "downloadBTN";
+            this.downloadBTN.Size = new System.Drawing.Size(125, 47);
+            this.downloadBTN.TabIndex = 0;
+            this.downloadBTN.Text = "Download Aysnc";
+            this.downloadBTN.UseVisualStyleBackColor = true;
+            this.downloadBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(22, 108);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(776, 332);
+            this.textBox1.TabIndex = 1;
+            // 
+            // cancelBTN
+            // 
+            this.cancelBTN.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cancelBTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cancelBTN.Location = new System.Drawing.Point(153, 22);
+            this.cancelBTN.Name = "cancelBTN";
+            this.cancelBTN.Size = new System.Drawing.Size(125, 47);
+            this.cancelBTN.TabIndex = 2;
+            this.cancelBTN.Text = "Cancel Operation";
+            this.cancelBTN.UseVisualStyleBackColor = false;
+            this.cancelBTN.Click += new System.EventHandler(this.cancelBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(810, 470);
+            this.Controls.Add(this.cancelBTN);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.downloadBTN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Download Websites";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downloadBTN;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button cancelBTN;
+    }
+}
+
---
+++ Downloader/WebsiteDownloaderFRM.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Downloader
+{
+    internal class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+
+    public partial class WebsiteDownloaderFRM : Form
+    {
+        private CancellationTokenSource s_cts = null;
+
+        private static readonly IReadOnlyList<string> Websites = new List<string> { "https://google.com", "https://www.hotmail.com", "https://github.com/", "https://www.gmail.com/", "https://facebook.com", "https://www.microsoft.com", "https://www.samsung.com/", "https://www.yahoo.com/" };
+
+        public WebsiteDownloaderFRM()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            cancelBTN.Enabled = false;
+            downloadBTN.Enabled = !downloadBTN.Enabled;
+
+            int count = 0;
+            s_cts = new CancellationTokenSource();
+            try
+            {
+                count = await RunGetParallelAsync(s_cts.Token);
+                downloadBTN.Enabled = !downloadBTN.Enabled;
+            }
+            catch
+            {
+            }
+            finally
+            {
+                textBox1.Text += $"Completed. {Environment.NewLine} Total Downloaded Content Lenght: {count}";
+            }
+        }
+
+        private async Task<int> RunGetParallelAsync(CancellationToken cancellationToken)
+        {
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (var item in Websites)
+            {
+                tasks.Add(GetWebsiteAsync(item, cancellationToken));
+            }
+            cancelBTN.Enabled = true;
+            try
+            {
+                await Task.WhenAll(tasks);
+            }
+            catch
+            {
+            }
+            return tasks.Where(x => x.IsCompletedSuccessfully).Sum(x => x.Result.WebsiteData?.Length) ?? 0;
+        }
+
+        private async Task<WebsiteDataModel> GetWebsiteAsync(string url, CancellationToken cancellationToken)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+
+            try
+            {
+                using HttpClient httpClient = new HttpClient(new HttpClientHandler
+                {
+                    Proxy = null,
+                    UseProxy = false
+                });
+                output.WebsiteUrl = url;
+                await Task.Delay(1200, cancellationToken); //  there are only 3 sites to be downloaded thats why I applied it here.
+                var resp = await httpClient.GetAsync(url, cancellationToken);
+                var string_resp = await resp.Content.ReadAsStringAsync();
+                output.WebsiteData = string_resp;
+                ReportWebsiteInfo(output);
+            }
+            catch (TaskCanceledException)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    textBox1.Text += $"{ output.WebsiteUrl } download cancelled.{ Environment.NewLine }";
+                });
+            }
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            Invoke((MethodInvoker)delegate
+            {
+                textBox1.Text += $"{ data.WebsiteUrl } downloaded: { data.WebsiteData.Length } characters long.{ Environment.NewLine }";
+            });
+        }
+
+        private void cancelBTN_Click(object sender, EventArgs e)
+        {
+          s_cts?.Cancel();
+        }
+    }
+}
